024ae7565f0c878e089fcd5fcc1d47d170ac0597
tazain-coder <btazain@gmail.com>
2025-12-21 16:27:00 +0600

    range sum

---
+++ Assiut University Trainer/rangeSum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    while (T--) {
+        long long L, R;
+        scanf("%lld %lld", &L, &R);
+
+        if (L > R) {
+            long long tmp = L;
+            L = R;
+            R = tmp;
+        }
+
+        long long n = R - L + 1;
+        long long sum = n * (L + R) / 2;
+
+        printf("%lld\n", sum);
+    }
+
+    return 0;
+}
