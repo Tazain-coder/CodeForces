03e0b2340af1b8023cc0e3eb3d94082b9a5a0205
tazain-coder <btazain@gmail.com>
2025-12-25 00:59:58 +0600

    strings update

--- Assiut University Trainer/strings.c
+++ Assiut University Trainer/strings.c
@@ -5,15 +5,10 @@ int main() {
     char s1[21];
     char s2[21];
 
-
     scanf("%s %s",s1,s2);
 
-    char c1[21],c2[21];
-    strcpy(c1,s1);
-    strcpy(c2,s2);
-
     printf("%d %d\n",strlen(s1),strlen(s2));
-    printf("%s\n",strcat(c1,c2));
+    printf("%s%s\n",s1,s2);
 
     char tmp;
     tmp = s1[0];
